86fd95faeed69d0b77e4faaabd55cf34d339a8cf
sinai <sinailei@gmail.com>
2023-11-07 09:17:10 +0200

    Added the function ImageClicked

--- app/src/main/java/com/example/firstthread/MainActivity.java
+++ app/src/main/java/com/example/firstthread/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.firstthread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ImageClicked(View view)
+    {
+        ImageView imageView = (ImageView)view;
+        imageView.setX(view.getX() + 10);
     }
 }
